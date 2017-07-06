f28307cd4737108dbcc8c8638ddf55ba9e706ae4
Yunyu Lin <yunyu.lin@vanderbilt.edu>
2017-07-06 08:11:33 -0600

    Port to xterm.js, support full screen

--- frontend/src/ShellTerm.vue
+++ frontend/src/ShellTerm.vue
@@ -1,67 +1,42 @@
 <template>
-    <div class="terminal-wrapper">
-        <div ref="terminal"></div>
+    <div class="terminal-wrapper" ref="terminal" v-resize:throttle="onResize">
     </div>
 </template>
 
 <style lang="scss">
+@import '../node_modules/xterm/src/xterm.css';
+
 .terminal-wrapper {
+    width: 100%;
+    height: 100%;
+
     .terminal {
         font-family: Menlo, Monaco, Consolas, monospace;
-    }
-
-    $term-color-black: #030303;
-    $term-color-fg: #b0b8c1;
-
-    /* TERM.JS STYLES */
-    .fg {
-        color: $term-color-fg;
-    }
-
-    div[style*=";color: rgb(240, 240, 240);"] {
-        color: $term-color-fg !important;
-    }
-
-    .bg {
-        background-color: $term-color-black;
-    }
-
-    div[style*="background-color: rgb(0, 0, 0);"] {
-        background-color: $term-color-black !important;
-    }
-
-    .terminal-cursor {
-        color: $term-color-black;
-        background-color: $term-color-fg;
-    }
-
-     ::-moz-selection {
-        color: $term-color-black;
-        background: #b4d5ff;
-    }
-
-     ::selection {
-        color: $term-color-black;
-        background: #b4d5ff;
     }
 }
 </style>
 
 <script>
 import VertxTerm from './vertx-shell.js';
+import resize from 'vue-resize-directive';
 
 export default {
     name: 'Shell',
+    directives: {
+        resize,
+    },
     mounted() {
         this.$nextTick(() => {
-            this.term = new VertxTerm(window.location.pathname + '/shellproxy', {
-                cols: 80,
-                rows: 24
-            }, this.$refs.terminal);
+            this.term = new VertxTerm(window.location.pathname + '/shellproxy', {}, this.$refs.terminal);
         });
     },
     beforeDestroy() {
         this.term.close();
+    },
+    methods: {
+        onResize(e) {
+            this.term.refit();
+        }
     }
 }
 </script>
